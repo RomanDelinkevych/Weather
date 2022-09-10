74a0e1e3884051745f25a976515b193692129371
Roman Delinkevych <delinkevich123@gmail.com>
2022-09-10 23:48:12 +0300

    Add window.navigation.getLocation()

--- main.js
+++ main.js
@@ -1,105 +1,59 @@
-
-
-let mainOutput = document.querySelector("#mainOutput");
 let mainInput = document.querySelector("#mainInput");
 let btnSearch = document.querySelector("#btnSearch");
+let btnNavigation = document.querySelector("#navigation");
+let clientTimeHour = document.querySelector(".hour");
+let clientTimeMonth = document.querySelector(".month");
+let weatherToday = document.querySelector("#todayWeather");
+let futureWeather = document.querySelector("#futureWeather");
+let placeCity = document.querySelector("#city");
+let placeCountry = document.querySelector("#country");
 
 class Weather {
-    constructor(url) {
-        this.url = url;
-        this.mainObj = undefined;
-        this.tempK = undefined;
+    constructor(mainObj) {
+        this.mainObj = mainObj;
         this.tempC = undefined;
-        this.tempF = undefined;
-        this.cityName = undefined;
-        this.clouds = undefined;
-        this.cloudsDescription = undefined;
     }
 
-    takeMainObject() {
-        fetch(this.url)
-            .then(response => response.json())
-            .then(json => {
-                this.mainObj = json;
-                this.cityName = this.mainObj.name;
-                this.clouds = this.mainObj.clouds.all;
-                this.cloudsDescription = this.mainObj.weather[0].description;
-                this.windSpead = this.mainObj.wind.speed;
-
-            })
-            .then(after => this.createInHTML());
+    calculateTemp() {
+        this.tempC = this.mainObj.main.temp - 273.15;
     }
 
     createInHTML() {
         // console.log(this.mainObj);
+
         this.calculateTemp();
-        let mainDiv = document.createElement("div");
-        mainDiv.classList.add("mainDiv");
-
-        let helpDivTemp = document.createElement("div");
-        helpDivTemp.classList.add("helpDivTemp");
-
-        let pName = document.createElement("p");
-        pName.textContent = this.cityName;
-
-        let pTemp = document.createElement("p");
-        pTemp.innerHTML = this.tempC + "&#8451";
-
-        let divTemp = document.createElement("div");
-        divTemp.classList.add("divTemp");
-
-        let btnC = document.createElement("button");
-        btnC.innerHTML = "&#8451";
-
-        let btnF = document.createElement("button");
-        btnF.innerHTML = "&#8457";
-
-        let divBtns = document.createElement("div");
-        divBtns.append(btnC);
-        divBtns.append(btnF);
-        divBtns.classList.add("divBtns");
-
-        divTemp.append(pTemp);
-        divTemp.append(divBtns);
-
-        divBtns.addEventListener("click", function (e) {
-            if (e.target.textContent === "℉")
-            {
-                console.log(this.tempF);
-                pTemp.innerHTML = this.tempF + "&#8457";
-            }
-            else if (e.target.textContent === "℃")
-            {
-                pTemp.innerHTML = this.tempC + "&#8451";
-            }
-
-
-        })
-
-        let pClouds = document.createElement("p");
-        pClouds.textContent = this.clouds + "% " + this.cloudsDescription;
-
-        let pWindSpeed = document.createElement("p");
-        pWindSpeed.textContent = this.windSpead + " metre/sec";
 
         let img = document.createElement("img");
         img.src = this.getImg();
 
-        helpDivTemp.append(pClouds);
+        let helpDivTemp = document.createElement("div");
+        helpDivTemp.classList.add("helpDivTemp");
 
-        mainDiv.append(pName);
-        mainDiv.append(divTemp);
-        mainDiv.append(helpDivTemp);
-        mainDiv.append(pWindSpeed);
-        mainDiv.append(img);
+        let pDay = document.createElement("p");
 
-        mainOutput.append(mainDiv);
-    }
+        let pTemp = document.createElement("p");
+        pTemp.innerHTML = Math.round(this.tempC) + "&#8451";
 
-    calculateTemp() {
-        this.tempK = this.mainObj.main.temp;
-        this.tempC = Math.round(this.tempK - 273.15);
-        this.tempF = Math.round(1.8 * this.tempC + 32);
+        if (weatherToday.childElementCount < 1) {
+            pDay.textContent = "TODAY";
+
+            helpDivTemp.append(pDay);
+            helpDivTemp.append(pTemp);
+
+            weatherToday.append(img);
+            weatherToday.append(helpDivTemp);
+        }
+        else {
+            let time = new Date(this.mainObj.dt_txt);
+            let arrTime = time.toString().split(" ");
+            pDay.textContent = arrTime[0];
+
+            helpDivTemp.append(pDay);
+            helpDivTemp.append(img);
+            helpDivTemp.append(pTemp);
+
+            futureWeather.append(helpDivTemp);
+        }
     }
 
     getImg() {
@@ -107,24 +61,35 @@ class Weather {
         let urlImg = `http://openweathermap.org/img/wn/${codeImg}@2x.png`;
         return urlImg;
     }
-
 }
 
 class urlService {
-    constructor(cityName) {
+    constructor(cityName, lon, lat) {
         this.cityName = cityName;
         this.lon = undefined;
         this.lat = undefined;
         this.id = undefined;
+        this.country = undefined;
+        if (lon && lat) {
+            this.lon = lon;
+            this.lat = lat;
+            this.getIdCity();
+        }
+
     }
 
-    getLatLonCity() {
+    getLatLonCity(e) {
         fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${this.cityName}&appid=d59a0c78a31e2f2917a6b6b76f643510`)
             .then(response => response.json())
             .then(json => {
+                // console.log(json);
+                this.cityName = json[0].name;
+                this.country = json[0].state;
+
                 this.lat = json[0].lat;
                 this.lon = json[0].lon;
-                this.getIdCity();
+
+                    this.getIdCity();
             })
     }
 
@@ -132,33 +97,207 @@ class urlService {
         fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${this.lat}&lon=${this.lon}&appid=d59a0c78a31e2f2917a6b6b76f643510`)
             .then(response => response.json())
             .then(json => {
-                this.id = json.id;
-                // this.getHistoryWeather();
-                this.create();
+                if (this.cityName === undefined) {
+                    this.cityName = json.name;
+                    this.getLatLonCity();
+                } else {
+                    console.log(json);
+                    this.id = json.id;
+                    this.getWeatherList();
+                }
             })
     }
 
-    getHistoryWeather() {
-        fetch(`https://pro.openweathermap.org/data/2.5/forecast/hourly?lat=44.34&lon=10.99&appid=bf35cac91880cb98375230fb443a116f`)
+    getWeatherList() {
+        fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${this.lat}&lon=${this.lon}&appid=d59a0c78a31e2f2917a6b6b76f643510`)
             .then(response => response.json())
-            .then(json => console.log(json));
+            .then(json => {
+                // console.log(json);
+                let time = new clientDate();
+                time.setTimeNow();
+                let monthNumber = time.month + 1;
+                let dayNumber = time.day + 1;
+                let arr = [];
+                arr.push(json.list[0]);
+                if (monthNumber < 10) {monthNumber = "0" + monthNumber;}
+                if (dayNumber < 10) {dayNumber = "0" + dayNumber;}
+
+                json.list.forEach(weatherDay => {
+                    let neededDate = time.year + "-" + monthNumber + "-" + dayNumber + " 12:00:00";
+                    if (futureWeather.childElementCount < 6) {
+                        if (weatherDay.dt_txt === neededDate) {
+                            dayNumber = dayNumber + 1;
+                            arr.push(weatherDay);
+                        }
+                    }
+                    else return;
+                })
+
+                placeCity.textContent = this.cityName;
+                placeCountry.textContent = this.country;
+
+                arr.forEach(day => {
+                    let cityWeather = new Weather(day);
+                    // this.consoleUrlService();
+                    cityWeather.createInHTML();
+                })
+
+            });
     }
 
-    create() {
-        let cityWeather = new Weather(`http://api.openweathermap.org/data/2.5/weather?id=${this.id}&appid=bf35cac91880cb98375230fb443a116f`);
-        cityWeather.takeMainObject();
-    }
+    consoleUrlService() {
+        console.log(`======consoleUrlService==========Start==========`);
 
+        console.log(`this.cityName = ${this.cityName}`);
+        console.log(`this.lon = ${this.lon}`);
+        console.log(`this.lat = ${this.lat}`);
+        console.log(`this.id = ${this.id}`);
+
+        console.log(`======consoleUrlService==========Finish==========`);
+    }
 }
+
+class clientDate {
+    constructor() {
+        this.today = undefined;
+        this.year = undefined;
+        this.month = undefined;
+        this.day = undefined;
+        this.hour = undefined;
+        this.minute = undefined;
+        this.second = undefined;
+        this.millisecond = undefined;
+        this.monthName = undefined;
+        this.dayName = undefined;
+        this.timeHourMinSecMillSec = undefined;
+        this.timeDayMonthYear = undefined;
+    }
+
+    setTimeNow() {
+        let time = new Date();
+        let year = time.getFullYear();
+        let month = time.getMonth();
+        let day = time.getUTCDate();
+        let hour = time.getHours();
+        let minute = time.getMinutes();
+        let second = time.getSeconds();
+        let millisecond = time.getMilliseconds();
+
+        this.today = time;
+        this.year = year;
+        this.month = month;
+        this.day = day;
+        this.hour = hour;
+        this.minute = minute;
+        this.second = second;
+        this.millisecond = millisecond;
+
+        this.getMonthName();
+        this.getDayName();
+
+        this.createTimeHourAndMinutes();
+        this.createTimeDayMonthYear();
+
+        //Виводить все із класу в консоль
+        // this.consoleTime();
+    }
+
+    getMonthName() {
+        let allMonth = ["January", "February", "March", "April",
+            "May", "June", "July", "August",
+            "September", "October", "November", "December"];
+        this.monthName = allMonth[this.month];
+    }
+
+    getDayName() {
+        let arrTime = this.today.toString().split(" ");
+        if (arrTime[0] === "Mon") this.dayName = "Monday";
+        else if (arrTime[0] === "Tue") {this.dayName = "Tuesday";}
+        else if (arrTime[0] === "Wed") {this.dayName = "Wednesday";}
+        else if (arrTime[0] === "Thu") {this.dayName = "Thursday";}
+        else if (arrTime[0] === "Fri") {this.dayName = "Friday";}
+        else if (arrTime[0] === "Sat") {this.dayName = "Saturday";}
+        else if (arrTime[0] === "Sun") {this.dayName = "Sunday";}
+    }
+
+    createTimeHourAndMinutes() {
+        this.timeHourMinSecMillSec = `${this.hour}:${this.minute}:${this.second}:${this.millisecond}`;
+    }
+
+    getTimeHourAndMinutes() {
+        this.setTimeNow();
+        return this.timeHourMinSecMillSec;
+    }
+
+    createTimeDayMonthYear() {
+        this.timeDayMonthYear = `${this.dayName}, ${this.day} ${this.monthName} ${this.year}`;
+    }
+
+    getTimeDayMonthYear() {
+        this.setTimeNow();
+        return this.timeDayMonthYear;
+    }
+
+    consoleTime() {
+        console.log(`this.today = ${this.today}`);
+        console.log(`this.year = ${this.year}`);
+        console.log(`this.month = ${this.month}`);
+        console.log(`this.day = ${this.day}`);
+        console.log(`this.hour = ${this.hour}`);
+        console.log(`this.minute = ${this.minute}`);
+        console.log(`this.second = ${this.second}`);
+        console.log(`this.millisecond = ${this.millisecond}`);
+        console.log(`this.dayName = ${this.dayName}`);
+        console.log(`this.monthName = ${this.monthName}`);
+        console.log(`this.timeHourMinSecMillSec = ${this.timeHourMinSecMillSec}`);
+        console.log(`this.timeDayMonthYear = ${this.timeDayMonthYear}`);
+    }
+}
+
+setInterval(function () {
+    let date = new clientDate();
+    let timeMessageHour = date.getTimeHourAndMinutes();
+    let timeMessageMonth = date.getTimeDayMonthYear();
+    clientTimeHour.innerHTML = "";
+    clientTimeMonth.innerHTML = "";
+    clientTimeHour.innerHTML = timeMessageHour;
+    clientTimeMonth.innerHTML = timeMessageMonth;
+}, 1)
+
+function startAPI() {
+    weatherToday.innerHTML = "";
+    futureWeather.innerHTML = "";
+    let newUrl = new urlService(mainInput.value);
+    newUrl.getLatLonCity();
+    mainInput.value = "";
+}
+
+btnSearch.addEventListener("click", function () {
+    console.log(mainInput.value.length);
+    if (mainInput.value.length > 0) {
+        startAPI();
+    }
+})
+
+btnNavigation.addEventListener("click", function () {
+    let x = navigator.geolocation.getCurrentPosition(start, f);
+    function start(e) {
+        weatherToday.innerHTML = "";
+        futureWeather.innerHTML = "";
+        let newUrl = new urlService(undefined, e.coords.longitude, e.coords.latitude);
+        mainInput.value = "";
+    }
+
+    function f() {
+        console.log("error");
+    }
+})
+
+document.addEventListener("keypress", function (e) {
+    if (e.key === "Enter" && mainInput.value.length > 0) {
+        startAPI();
+    }
+})
 
 let london = new urlService("London");
 london.getLatLonCity();
-let kyiv = new urlService("Kyiv");
-kyiv.getLatLonCity();
-let newYork = new urlService("New York");
-newYork.getLatLonCity();
-
-btnSearch.addEventListener("click", function () {
-    let newUrl = new urlService(mainInput.value);
-    newUrl.getLatLonCity();
-})
